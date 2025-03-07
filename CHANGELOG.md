0ed84b82aaf120b39fcea3e1f43c7c0809fccc86
烟雀 <yanquer@qq.com>
2025-03-07 10:41:16 +0800

    增加 changelog 与 设置版本号

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG 
+
+
+## 9.5.0-a
+
+- 增加 changelog 与 设置版本号
+- 从主分支main变基
+
+
+
+
---
+++ resources/scripts/auto-set-version.js
@@ -0,0 +1,43 @@
+
+// rst-parser
+const fs = require('fs');
+const path = require('path');
+
+const projectDir = path.join(__dirname, '..', '..');
+
+const getLatest = () => {
+  const readmePath = path.join(projectDir, 'CHANGELOG.md');
+  const readmeContent = fs.readFileSync(readmePath, 'utf8');
+
+  // 顶级标题第一个
+  const versionRegex = /#+\s+(\d+\.\d+\.\d+-[a-zA-Z]*)\s*/;
+  const versionMatch = readmeContent.match(versionRegex);
+
+  if (!versionMatch) {
+    console.error('无法从 CHANGELOG.md 中提取版本号');
+    process.exit(1);
+  }
+
+  return versionMatch[1];
+}
+
+
+const updateVersion = () => {
+  const newVersion = getLatest();
+  if (newVersion){
+    const packageJsonPath = path.join(projectDir, 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    if (packageJson.version === newVersion) {
+      return
+    }
+    packageJson.version = newVersion;
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log(`成功更新 package.json 中的版本号为: ${newVersion}`);
+  }
+}
+
+updateVersion()
+
+
+
+
